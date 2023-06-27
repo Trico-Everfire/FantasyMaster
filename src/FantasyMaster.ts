8392c23d3daf25a0f42c2a8dbf1fdeb030ed8671
jakobg1215 <jakobg1215@gmail.com>
2023-06-27 18:27:59 -0500

    Make FantasyMaster Extend Client

--- src/FantasyMaster.ts
+++ src/FantasyMaster.ts
@@ -4,39 +4,43 @@ import { WebSocketManager } from '@discordjs/ws';
 import ClientConfiguerations from './ClientConfiguerations';
 import CommandManager from './CommandManager';
 
-class FantasyMaster {
-    private readonly rest = new REST({ version: ClientConfiguerations.API_VERSION }).setToken(ClientConfiguerations.CLIENT_TOKEN);
-    private readonly gateway = new WebSocketManager({
-        intents: ClientConfiguerations.CLIENT_INTENT,
-        rest: this.rest,
-        token: ClientConfiguerations.CLIENT_TOKEN,
-        version: ClientConfiguerations.API_VERSION,
-    });
-    private readonly client = new Client({ gateway: this.gateway, rest: this.rest }); // TODO: extend this
-    private readonly commands = new CommandManager();
+class FantasyMaster extends Client {
+    private readonly gatewayAPI: WebSocketManager;
+    private readonly commandManager = new CommandManager();
     private userData!: APIUser;
 
     public constructor() {
-        this.client.once(GatewayDispatchEvents.Ready, ({ data }) => {
+        const rest = new REST({ version: ClientConfiguerations.API_VERSION }).setToken(ClientConfiguerations.CLIENT_TOKEN);
+        const gateway = new WebSocketManager({
+            intents: ClientConfiguerations.CLIENT_INTENT,
+            rest,
+            token: ClientConfiguerations.CLIENT_TOKEN,
+            version: ClientConfiguerations.API_VERSION,
+        });
+
+        super({ gateway, rest });
+        this.gatewayAPI = gateway;
+
+        this.once(GatewayDispatchEvents.Ready, ({ data }) => {
             this.userData = data.user;
             console.log('Bot is ready');
         });
 
-        this.client.on(GatewayDispatchEvents.InteractionCreate, ({ data: interaction, api }) => {
+        this.on(GatewayDispatchEvents.InteractionCreate, ({ data: interaction, api }) => {
             if (interaction.type === InteractionType.ApplicationCommand) {
-                this.commands.runCommand(interaction, api);
+                this.commandManager.runCommand(interaction, api);
                 return;
             }
         });
     }
 
     public async startBot() {
-        await this.commands.loadCommands();
-        await this.gateway.connect();
-        await this.commands.registerCommands(this.client.api, this.user.id);
+        await this.commandManager.loadCommands();
+        await this.gatewayAPI.connect();
+        await this.commandManager.registerCommands(this.api, this.user.id);
     }
 
-    // This is not undefined as the bot will always be logged in.
+    // This should be used only after the bot is connected.
     public get user() {
         return this.userData;
     }
